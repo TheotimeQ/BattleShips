0d7a1594462c087a09ec661a6661aa91b54d033b
Thomas POYETON <t.poyeton@eleve.leschartreux.net>
2023-05-04 09:57:51 +0200

    Abandon de l'ajout des pubs

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -7,31 +7,13 @@ import {
 } from '@tanstack/react-query';
 import { ChakraProvider } from '@chakra-ui/react';
 
-import Script from 'next/script';
-
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <Adsense
-        client_id="ca-pub-4238647462189874"
-      />
-
       <ChakraProvider>
         <Component {...pageProps} />
       </ChakraProvider>
     </QueryClientProvider>);
-}
-
-function Adsense(props: any) {
-  const { client_id } = props;
-
-  return (
-    <Script
-      strategy="lazyOnload"
-      src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${client_id}`}
-      crossOrigin="anonymous"
-    />
-  );
 }
